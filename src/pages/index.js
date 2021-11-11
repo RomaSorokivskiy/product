2d1d404a7a1bea738f6ee4aff34fff6a4aaaefa5
sorok <sorokivroma@gmail.com>
2021-11-11 23:11:55 +0300

    Normalize proj, and start navbar.js

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import {Box, Container, Heading, Link, useColorMode, UseColorModeValue} from '@chakra-ui/react'
+
+import logo from '../img/logo.png'
+
+
+const Page = () =>{
+    return(
+        <Box></Box>
+    )
+}
+export default Page
+
